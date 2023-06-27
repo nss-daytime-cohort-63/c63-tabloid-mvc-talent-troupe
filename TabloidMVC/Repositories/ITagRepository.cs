da863b624ff2e640e490a1ff52ca7c75c85be6cb
Alex Bishop <alexb98@gmail.com>
2023-06-27 13:22:54 -0500

    Updated Tag Management page to show all tags alphabetically. Updated _Layout to have proper routes

---
+++ TabloidMVC/Repositories/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ITagRepository
+    {
+
+        List<Tag> GetAllTags();
+
+    }
+}
---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using TabloidMVC.Models;
+using System.Linq;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : ITagRepository 
+    {
+        private string _connectionString;
+        public TagRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        protected SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+        public List<Tag> GetAllTags()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Tag ORDER BY Name ASC";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Tag> tags = new List<Tag>();
+                        while (reader.Read())
+                        {
+                            Tag tag = new Tag()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+
+                            tags.Add(tag);
+                        }
+                        return tags;
+                    }
+                }
+            }
+        }
+
+    }
+}
