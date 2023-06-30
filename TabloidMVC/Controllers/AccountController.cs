0246f3828212ab06434b3061907438139189600f
sergey-mik <intelsurge@gmail.com>
2023-06-30 13:49:24 -0500

    approved_un-approved

--- TabloidMVC/Controllers/AccountController.cs
+++ TabloidMVC/Controllers/AccountController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using TabloidMVC.Models;
